e037a826daf588fe54aa2e2a943dc73c30701035
Jesli <jeslipriya07@gmail.com>
2026-07-17 20:27:38 +0530

    added

--- src/numbers/LCM.java
+++ src/numbers/LCM.java
@@ -1,10 +1,23 @@
 package numbers;
 
+import java.util.Scanner;
+
 public class LCM {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int lcm = (a * b) / GCD.gcd(a, b);
+
+        System.out.println("LCM = " + lcm);
+
+        sc.close();
+    }
 }
